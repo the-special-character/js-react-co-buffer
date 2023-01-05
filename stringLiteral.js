9bdeef87f2c595e9d3287d6a2892ed96997c0bcb
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-01-05 15:58:02 +0530

    completed scope + function + stringLiteral

---
+++ stringLiteral.js
@@ -0,0 +1,44 @@
+const firstName = "Yagnesh";
+
+const lastName = "Modh";
+
+// const fullName = firstName + ' ' + lastName;
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+// yagnesh's car
+
+// const pos = firstName + '\'s car';
+console.log(`${firstName}'s car`);
+
+// console.log(pos);
+
+// const html = '<!DOCTYPE html>' +
+// '\n<html lang="en">' +
+// '\n<head>' +
+//     '\n\t<meta charset="UTF-8">' +
+//     '\n\t<meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+//     '\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+//     '\n\t<title>Document</title>' +
+// '\n</head>' +
+// '\n<body>' +
+//     '\n' +
+// '\n</body>' +
+// '\n</html>';
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>`;
+
+console.log(html);
