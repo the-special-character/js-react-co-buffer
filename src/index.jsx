4d279bcfcc791feae6aef7a380c9110119baf387
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-21 16:07:42 +0530

    commit code

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+// import App from './app';
 import ErrorBoundary from './errorBoundary';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container);
 
+function App({ number }) {
+  const [counter, setCounter] = useState(number);
+  const [name, setName] = useState('Yagnesh');
+  const isMounted = useRef(false);
+
+  // Clousures
+  const increment = () => {
+    setCounter((val) => val + 1);
+  };
+
+  const decrement = () => {
+    setCounter((val) => val - 1);
+  };
+
+  const changeName = () => {
+    setName('Rohit');
+  };
+
+  // Never Change Value of dependency
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log('Change value');
+    }
+  }, [name]);
+
+  // Component Did Mount
+  // Component Did Update  // **Never write useEffect without dependency**
+  useEffect(() => {
+    console.log('Component Did Mount');
+    isMounted.current = true;
+  }, []);
+
+  return (
+    <>
+      <div>
+        <button type="button" onClick={increment}>
+          +
+        </button>
+        {counter}
+        <button type="button" onClick={decrement}>
+          -
+        </button>
+      </div>
+      <div>
+        {name}
+        <button type="button" onClick={changeName}>
+          Change Name
+        </button>
+      </div>
+    </>
+  );
+}
+
 root.render(
   <ErrorBoundary>
-    <App />
+    <App number={10} />
   </ErrorBoundary>,
 );
